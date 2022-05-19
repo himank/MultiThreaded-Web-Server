a1ba74793bbfcf1308cbdb89ec014f66fd7d82f5
Himank Chaudhary <himank.tomar@gmail.com>
2022-05-19 15:33:53 -0700

    headers

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,10 +1,18 @@
-//============================================================================
-// Name        : parse.cpp
+// Copyright 2022 @HimankChaudhary
+//
 // Author      : Himank Chaudhary
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Parsing of request and mainting client request & then giving this request to a particular thread based on the scheduling policy
-//============================================================================
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <iostream>
 #include <stdio.h>
